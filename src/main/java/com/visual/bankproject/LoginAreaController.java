48657634f2ad4e799209c65b5396b303e9835fe5
pninci13 <p.ninci13@gmail.com>
2021-10-14 21:30:06 -0300

    manager features v1

--- src/main/java/com/visual/bankproject/LoginAreaController.java
+++ src/main/java/com/visual/bankproject/LoginAreaController.java
@@ -21,83 +21,75 @@ public class LoginAreaController {
     private Parent root;
 
     @FXML
-    private TextField emailText;
+    private TextField usernameText;
 
     @FXML
     private PasswordField passwordText;
 
     public void switchToRegisterArea(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("signupArea.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToRespectiveArea(ActionEvent event) throws IOException {
-        if (emailText.getText().contains("@manager-acc.com")) {
-            if (loginValidation(emailText.getText(), passwordText.getText())) {
-                root = FXMLLoader.load(getClass().getResource("managerArea.fxml"));
-                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                scene = new Scene(root, 600, 400);
-                stage.setScene(scene);
-                stage.show();
-            } else {
-                Alert message = new Alert(Alert.AlertType.INFORMATION);
-                message.setContentText("Invalid Login");
-                message.setTitle("Unsuccessfull Login");
-                message.show();
-                passwordText.setText("");
-            }
-        }else if (emailText.getText().contains("@client-acc.com")) {
-            if (loginValidation(emailText.getText(), passwordText.getText())) {
-                root = FXMLLoader.load(getClass().getResource("clientArea.fxml"));
-                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                scene = new Scene(root, 600, 400);
-                stage.setScene(scene);
-                stage.show();
-            }else{
-                Alert message = new Alert(Alert.AlertType.INFORMATION);
-                message.setContentText("Invalid Login");
-                message.setTitle("Unsuccessfull Login");
-                message.show();
-                passwordText.setText("");
-            }
-        }else{
+        if (loginValidation(usernameText.getText(), passwordText.getText()) == 0) {
             Alert message = new Alert(Alert.AlertType.INFORMATION);
             message.setContentText("Invalid Login");
             message.setTitle("Unsuccessfull Login");
             message.show();
             passwordText.setText("");
+        } else if (loginValidation(usernameText.getText(), passwordText.getText()) == 1) {
+            root = FXMLLoader.load(getClass().getResource("clientArea.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root, 600, 400);
+            stage.setScene(scene);
+            stage.show();
+        } else if (loginValidation(usernameText.getText(), passwordText.getText()) == 2) {
+            root = FXMLLoader.load(getClass().getResource("managerArea.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root, 600, 400);
+            stage.setScene(scene);
+            stage.show();
         }
     }
 
-    public boolean loginValidation(String email, String password) throws IOException {
-        File file = new File("com/visual/bankproject/bankUserInformation.txt");
+    public int loginValidation(String username, String password) throws IOException {
+        File file = new File("src\\main\\java\\com\\visual\\bankproject\\bankUserInformation.txt");
 
-        if((email == null) && (password == null)){
-            return false;
+        int type = 0;
+
+        if (username.isEmpty() || password.isEmpty()) {
+            return type;
         }
 
         Scanner scan = new Scanner(file);
         String aux;
 
-        if(email.isEmpty() || password.isEmpty()){
-            return false;
-        }
 
-        while(scan.hasNextLine()){
+        while (scan.hasNextLine()) {
             aux = scan.nextLine();
-            if (aux.equals(email)) {
+            if (aux.equals("Client")) {
+                type = 1;
+            } else if (aux.equals("Manager")) {
+                type = 2;
+            }
+
+            if (scan.nextLine().equals(username)) {
                 if (password.equals(scan.nextLine())) {
-                    return true;
+                    return type;
                 } else {
                     break;
                 }
             }
+
+            scan.nextLine();
         }
 
-        return false;
+        type = 0;
+        return type;
     }
 
 }
